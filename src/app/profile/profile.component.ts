5e15366cc5bec4fe98aa7775deb4502d5381e4ce
tahir94 <tahirkhan.on@gmail.com>
2017-11-29 04:49:09 +0500

    woking on mini nav

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	isPassword : Boolean = false;
+	isProfile : Boolean = true;
+  constructor(private router : Router) { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
+  selectOption(){
+	  console.log('select');
+	  
+  }
 
+nav(){
+	console.warn('OKUJLKAJDSLKJASLKDJALSJDLK')
+	this.router.navigate(['home/password'])
+}
+
+  navigateTo(event){
+	console.log(event);
+	switch(event){
+		case 'password':
+		console.log('asdasdsa');
+		console.log(this.router.url);
+		this.router.navigate(['home/password'])
+		this.isPassword = true;
+		this.isProfile = false;
+
+		default:
+		return null;	
+	}
+  }
 }

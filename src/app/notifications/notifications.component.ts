b233a68f1cf965a0299719852b1bf26e83fc3b84
tahir94 <tahirkhan.on@gmail.com>
2017-11-29 05:40:18 +0500

    stuck in mini mav issue

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,15 +1,166 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent implements OnInit {
-
-  constructor() { }
+	isProfile: Boolean = false;
+  isPassword: Boolean = false;
+  isNotifications: Boolean = true;
+  isBillingInfo: Boolean = false;
+  isGiftCards: Boolean = false;
+  isCancelTask: Boolean = false;
+  isAccountBalance: Boolean = false;
+  isTransactions: Boolean = false;
+  isAlexa: Boolean = false;
+	isDeactivate: Boolean = false;
+  constructor(private router : Router) { }
 
   ngOnInit() {
   }
-
+  navigateTo(event){
+    console.log(event);
+    switch(event){
+      case 'password':
+      this.router.navigate(['home/password']);
+  
+      this.isPassword = true;
+  
+      this.isProfile = false;
+      this.isNotifications = false;
+      this.isBillingInfo = false;
+      this.isGiftCards = false;
+      this.isCancelTask = false;
+      this.isAccountBalance = false;
+      this.isTransactions = false;
+      this.isAlexa = false;
+      this.isDeactivate = false;
+  
+  
+      case 'notification':
+      this.router.navigate(['home/app-notifications']);
+      
+          this.isNotifications = true;
+  
+          this.isPassword = false;		
+          this.isProfile = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'billingInfo':
+      this.router.navigate(['home/app-billing-info']);
+      
+          this.isBillingInfo = true;
+  
+          this.isPassword = false;		
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'giftCards':
+      this.router.navigate(['home/app-gift-cards']);
+      
+          this.isGiftCards = true;
+  
+          this.isPassword = false;	
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'cancelTask':
+      this.router.navigate(['home/app-cancel-task']);
+      
+          this.isCancelTask = true;
+  
+          this.isPassword = false;	
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'accountBalaance':
+      this.router.navigate(['home/app-account-balance']);
+      
+          this.isAccountBalance = true;
+  
+          this.isPassword = false;	
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'transactions':
+      this.router.navigate(['home/app-transactions']);
+      
+          this.isTransactions = true;
+  
+          this.isPassword = false;
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isAlexa = false;
+          this.isDeactivate = false;
+  
+      case 'alexa':
+      this.router.navigate(['home/app-alexa']);
+      
+          this.isAlexa = true;
+  
+          this.isPassword = false;	
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isDeactivate = false;
+  
+      case 'deactivate':
+      this.router.navigate(['home/app-deactive']);
+      
+      this.isDeactivate = true;
+         
+      this.isPassword = false;
+          this.isProfile = false;
+          this.isNotifications = false;
+          this.isBillingInfo = false;
+          this.isGiftCards = false;
+          this.isCancelTask = false;
+          this.isAccountBalance = false;
+          this.isTransactions = false;
+          this.isAlexa = false;
+  
+      default:
+      return null;	
+    }
+    }
 }
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,33 +6,165 @@ import { Router } from "@angular/router";
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-	isPassword : Boolean = false;
-	isProfile : Boolean = true;
+	isProfile: Boolean = true;
+  isPassword: Boolean = false;
+  isNotifications: Boolean = false;
+  isBillingInfo: Boolean = false;
+  isGiftCards: Boolean = false;
+  isCancelTask: Boolean = false;
+  isAccountBalance: Boolean = false;
+  isTransactions: Boolean = false;
+  isAlexa: Boolean = false;
+	isDeactivate: Boolean = false;
+	
   constructor(private router : Router) { 
 
   }
 
   ngOnInit() {
   }
-  selectOption(){
-	  console.log('select');
-	  
-  }
 
-nav(){
-	console.warn('OKUJLKAJDSLKJASLKDJALSJDLK')
-	this.router.navigate(['home/password'])
-}
+
+
 
   navigateTo(event){
 	console.log(event);
 	switch(event){
 		case 'password':
-		console.log('asdasdsa');
-		console.log(this.router.url);
-		this.router.navigate(['home/password'])
+		this.router.navigate(['home/password']);
+
 		this.isPassword = true;
+
 		this.isProfile = false;
+		this.isNotifications = false;
+		this.isBillingInfo = false;
+		this.isGiftCards = false;
+		this.isCancelTask = false;
+		this.isAccountBalance = false;
+		this.isTransactions = false;
+		this.isAlexa = false;
+		this.isDeactivate = false;
+
+
+		case 'notification':
+		this.router.navigate(['home/app-notifications']);
+		
+				this.isNotifications = true;
+
+				this.isPassword = false;		
+				this.isProfile = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'billingInfo':
+		this.router.navigate(['home/app-billing-info']);
+		
+				this.isBillingInfo = true;
+
+				this.isPassword = false;		
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'giftCards':
+		this.router.navigate(['home/app-gift-cards']);
+		
+				this.isGiftCards = true;
+
+				this.isPassword = false;	
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'cancelTask':
+		this.router.navigate(['home/app-cancel-task']);
+		
+				this.isCancelTask = true;
+
+				this.isPassword = false;	
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'accountBalaance':
+		this.router.navigate(['home/app-account-balance']);
+		
+				this.isAccountBalance = true;
+
+				this.isPassword = false;	
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'transactions':
+		this.router.navigate(['home/app-transactions']);
+		
+				this.isTransactions = true;
+
+				this.isPassword = false;
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isAlexa = false;
+				this.isDeactivate = false;
+
+		case 'alexa':
+		this.router.navigate(['home/app-alexa']);
+		
+				this.isAlexa = true;
+
+				this.isPassword = false;	
+				this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isDeactivate = false;
+
+		case 'deactivate':
+		this.router.navigate(['home/app-deactive']);
+		
+		this.isDeactivate = true;
+			 
+		this.isPassword = false;
+      	this.isProfile = false;
+				this.isNotifications = false;
+				this.isBillingInfo = false;
+				this.isGiftCards = false;
+				this.isCancelTask = false;
+				this.isAccountBalance = false;
+				this.isTransactions = false;
+				this.isAlexa = false;
 
 		default:
 		return null;	
